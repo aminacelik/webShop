7f69870dd5a7267a48496d4cb6aa240120be5ad9
aminacelik <celik.amina@gmail.com>
2014-11-05 16:04:03 +0100

    Prettified error messages on user registration page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
 	
 	
+def errors_for(model, attribute)
+  if model.errors[attribute].present?
+    content_tag :span, :class => 'error_explanation' do
+      model.errors[attribute].first
+    end
+  end
+end
+
+
 end
