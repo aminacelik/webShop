0ebd33da74b726b0edcca89bec8c44cda3fe3645
aminacelik <celik.amina@gmail.com>
2014-12-22 19:32:05 +0100

    Code refctoring, enabled ajax deleting items on 3rd purchase step

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20141222115645) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "addresses", force: true do |t|
     t.string   "street_name"
     t.integer  "street_number"
